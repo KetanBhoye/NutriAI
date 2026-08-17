d158e7d3d6d1d7b0b165d1146b1a557bdf32ecdd
Ketan <imaketanbhoye100@gmail.com>
2026-08-17 07:36:30 +0530

    Release Android v1.0.12

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -31,7 +31,7 @@ const HEALTH_CONNECT_PERMISSIONS = [
 const config: ExpoConfig = {
   name: 'NutriAI',
   slug: 'nutriai-mobile',
-  version: '1.0.11',
+  version: '1.0.12',
   orientation: 'portrait',
   scheme: 'nutriai',
   userInterfaceStyle: 'dark',
@@ -62,7 +62,7 @@ const config: ExpoConfig = {
      * from another, and some installers refuse to update at all. `npm run
      * release` bumps both this and `version` for you.
      */
-    versionCode: 12,
+    versionCode: 13,
     permissions: [
       ...HEALTH_CONNECT_PERMISSIONS,
       /**
